05818016b03f5027a5a8493134ff4d4f66059be0
Mats Andersson <mats.olof.andersson@gmail.com>
2023-09-17 18:13:24 +0200

    Added placeholder random

--- app/1_Kimera.py
+++ app/1_Kimera.py
@@ -1,11 +1,20 @@
 import streamlit as st
 import plotly.express as px
 import pandas as pd
+import numpy as np
 
 @st.cache_data()
 def getWeapons(file :str) -> pd.DataFrame:
     return pd.read_csv(file)
 
+row = 5
+cols = 5
 
+Random = np.random.randint(low=0, high=100, size=(row, cols))
+
+df = pd.DataFrame(Random)
 
 st.write("Hej")
+st.write(df)
+
+st.plotly_chart(px.scatter(df, x=0, y=1))
