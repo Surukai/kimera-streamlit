261f0779d0cb25dfe3cbe3d44f9cb3d7d42ed81f
Mats Andersson <mats.olof.andersson@gmail.com>
2023-11-05 15:09:10 +0100

    Old Guard Method

--- app/1_Kimera.py
+++ app/1_Kimera.py
@@ -39,63 +39,81 @@ def d_df(*dice):
 # Defaults
 df_hit = d_df(8, 8)
 df_dmg = d_df(8, 8)
-armor_layer1 = {'layer': "Reinforced", 'coverage': 4, 'protection': 8}
-#armor_layer_2 = {'layer': "Light armor", 'coverage': 8, 'protection': 4}
-df_armor = pd.DataFrame([armor_layer1])
+armor_layer1 = {'layer': "Blocked", 'coverage': 8, 'protection': 8}
+armor_layer2 = {'layer': "Light armor", 'coverage': 11, 'protection': 2}
+df_armor = pd.DataFrame([armor_layer1, armor_layer2])
+#st.write(df_armor)
 
 # Attack functions
-def attackMelee(df_hit=df_hit, df_dmg=df_dmg, guard=8, df_armor=df_armor, tough=8):
-    # make df of outcomes:
-    # damage outcomes as x-axis
-    # fraction/percentage as y-axis
-    # vertical stop-line
-    # plots: all clean dmgs, all layer dmgs, all total damages
-
+def attackMelee(df_hit=df_hit, df_dmg=df_dmg, guard=0, df_armor=df_armor, tough=8):
     #deduct guard from df_hit.result
     df_hit.result -= guard
     fraction_blocked = df_hit[df_hit.result <= 0].fraction.sum()
-    fraction_layer1 = df_hit[(df_hit.result <= armor_layer1['coverage']) & (0 < df_hit.result)].fraction.sum()
-    fraction_clean = df_hit[df_hit.result > armor_layer1['coverage']].fraction.sum()
-
-    st.write("fraction_blocked:", fraction_blocked)
-    st.write("fraction_layer1:", fraction_layer1)
-    st.write("fraction_clean:", fraction_clean)
-    st.write("checksum:", fraction_blocked + fraction_layer1 + fraction_clean)
-
-    df_dmg_layer1 = df_dmg.copy()
-    df_dmg_layer1['fraction'] = df_dmg_layer1['fraction'] * fraction_layer1
-    df_dmg_layer1['result'] = df_dmg_layer1['result'] - armor_layer1['protection']
+    dict_layer_fractions = {}
+    dict_outcome = {}
+    last_coverage = 0
+    for row in df_armor.iterrows():
+        layer = row[1]['layer']
+        coverage = row[1]['coverage']
+        protection = row[1]['protection']
+        armor_hit_fraction = df_hit[(df_hit.result <= coverage) & (last_coverage < df_hit.result)].fraction.sum()
+        # calculate damage for this layer
+        df_dmg_layer = df_dmg.copy()
+        df_dmg_layer['result'] -= protection
+        df_dmg_layer['fraction'] = df_dmg_layer['fraction'] * armor_hit_fraction
+        dict_layer_fractions[layer] = armor_hit_fraction
+        dict_outcome[layer] = df_dmg_layer
+        last_coverage = coverage
+    # that which didn't strike any armor is "clean"
+    fraction_clean = df_hit[df_hit.result > last_coverage].fraction.sum()
     df_dmg_clean = df_dmg.copy()
     df_dmg_clean['fraction'] = df_dmg_clean['fraction'] * fraction_clean
+    dict_outcome['clean'] = df_dmg_clean
 
-    result_sum = pd.DataFrame()
-    result_sum['result'] = pd.concat([df_dmg_clean['result'], df_dmg_layer1['result']]).unique()
+    # Initialize df_sum with a copy of the first DataFrame in dict_outcome
+    df_sum = dict_outcome[list(dict_outcome.keys())[0]].copy()
+    # Iterate through each key (DataFrame) in dict_outcome
+    for key, df in dict_outcome.items():
+        if key != list(dict_outcome.keys())[0]:
+            # Group the DataFrame by 'result' and sum the 'fraction' values
+            grouped = df.groupby('result')['fraction'].sum().reset_index()
+            # Merge the grouped DataFrame with df_sum using 'result' as the key
+            df_sum = pd.merge(df_sum, grouped, on='result', how='outer', suffixes=('', f'_{key}'))
+    # Calculate the sum of 'fraction' columns from different layers per result
+    df_sum['fraction'] = df_sum.filter(like='fraction').sum(axis=1)
+    # Sort the df_sum DataFrame by the 'result' column
+    df_sum = df_sum.sort_values(by='result').reset_index(drop=True)
+    dict_outcome['sum'] = df_sum
 
-    # Initialize the 'fraction_sum' column with zeros of type float
-    result_sum['fraction_sum'] = 0.0
-
-    # Calculate the sum of fractions for each 'result' that exists in both DataFrames
-    for result in result_sum['result']:
-        if result in df_dmg_clean['result'].unique():
-            result_sum.loc[result_sum['result'] == result, 'fraction_sum'] += df_dmg_clean[df_dmg_clean['result'] == result]['fraction'].sum()
-        if result in df_dmg_layer1['result'].unique():
-            result_sum.loc[result_sum['result'] == result, 'fraction_sum'] += df_dmg_layer1[df_dmg_layer1['result'] == result]['fraction'].sum()
-    result_sum = result_sum.sort_values(by='result')
-    # Create a line plot with df_dmg_clean
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=df_dmg_clean['result'], y=df_dmg_clean['fraction'], mode='lines', name='df_dmg_clean'))
-    # Create a line plot for df_dmg_layer1 and set the legend label to 'df_dmg_layer1' without an underscore
-    fig.add_trace(go.Scatter(x=df_dmg_layer1['result'], y=df_dmg_layer1['fraction'], mode='lines', name='df_dmg_layer1'))
-    # Create a line plot for the sum of fractions
-    fig.add_trace(go.Scatter(x=result_sum['result'], y=result_sum['fraction_sum'], mode='lines', name='Sum of Fractions'))
+    for key, df in dict_outcome.items():
+        fig.add_trace(go.Scatter(x=df['result'], y=df['fraction'], mode='lines', name=key))
 
     # Create vertical dashed lines at 0 and a specific value (e.g., tough)
     fig.add_shape(go.layout.Shape(type="line", x0=0, x1=0, xref="x", y0=0, y1=1, yref="paper", line=dict(color="red", width=2, dash="dash"), name='Zero'))
     fig.add_shape(go.layout.Shape(type="line", x0=tough, x1=tough, xref="x", y0=0, y1=1, yref="paper", line=dict(color="blue", width=2, dash="dash"), name='Tough Value'))
-
     # Show the combined plot
     st.plotly_chart(fig)
-       
+
+    # Calculate 'no_stop' and 'stopped' based on 'tough'
+    no_stop = df_sum[df_sum['result'] <= tough]['fraction'].sum()
+    stopped = df_sum[tough < df_sum['result']]['fraction'].sum()
+
+    # print distribution of hits:
+    st.write(f"blocked: {fraction_blocked*100:.2f}%")
+    struck_armor = 0
+    for key, value in dict_layer_fractions.items():
+        st.write(f"struck {key}: {value*100:.2f}%")
+        struck_armor += value
+    st.write(f"clean: {fraction_clean*100:.2f}%")
+    st.write(f"checksum struck: {(fraction_blocked+struck_armor+fraction_clean)*100:.2f}%")
+    st.write("")
+
+    # Summarize outcomes:
+    st.write(f"Blocked: {fraction_blocked*100:.2f}%")
+    st.write(f"No Stop: {no_stop*100:.2f}%")
+    st.write(f"Stopped: {stopped*100:.2f}%")       
+    st.write(f"checksum damage: {(fraction_blocked+no_stop+stopped)*100:.2f}%")
 
 # Specific instructions
 st.write("Professional soliders, armed, light armor, mutual combat, one strike:")
